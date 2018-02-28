4bcadfe73aa9ac38aa81a73b604b7431d35072c9
Sifan Liu <SLiu@brookings.edu>
2018-02-28 15:39:03 -0500

    Create R shiny App

---
+++ mpp_maps/app.R
@@ -0,0 +1,67 @@
+
+# README ------------------------------------------------------------------
+
+# Sifan Liu
+
+# KEY FUNCTIONS to be developed
+
+# 1. File upload: https://shiny.rstudio.com/gallery/file-upload.html
+# 2. Choose the variable to map for bubble size
+# 3. Choose the variable to map for bubble color
+# 4. Adjust the size
+# 5. Choose from the color template: https://drsimonj.svbtle.com/creating-corporate-colour-palettes-for-ggplot2
+
+
+# Shiny R -----------------------------------------------------------------
+
+
+
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+   
+   # Application title
+   titlePanel("100 largest metro Bubble maps"),
+   
+   # Sidebar with a slider input for number of bins 
+   sidebarLayout(
+      sidebarPanel(
+         sliderInput("bins",
+                     "Number of bins:",
+                     min = 1,
+                     max = 50,
+                     value = 30)
+      ),
+      
+      # Show a plot of the generated distribution
+      mainPanel(
+         plotOutput("distPlot")
+      )
+   )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+   
+   output$distPlot <- renderPlot({
+      # generate bins based on input$bins from ui.R
+      x    <- faithful[, 2] 
+      bins <- seq(min(x), max(x), length.out = input$bins + 1)
+      
+      # draw the histogram with the specified number of bins
+      hist(x, breaks = bins, col = 'darkgray', border = 'white')
+   })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
