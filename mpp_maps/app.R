e6b29cbe142c5ead8e67a7d4915f9987d493e902
fansi-sifan <SLiu@brookings.edu>
2018-03-05 10:04:51 -0500

    allow color pick

--- mpp_maps/app.R
+++ mpp_maps/app.R
@@ -5,9 +5,11 @@
 
 # KEY FUNCTIONS to be developed
 
+# https://shiny.rstudio.com/tutorial/written-tutorial/lesson5/
+
 # 1. ( ) File upload: https://shiny.rstudio.com/gallery/file-upload.html
 # 2. ( ) Choose the variable to map for bubble size
-# 3. (/) Choose the variable to map for bubble color
+# 3. (/) Choose the variable to map for bubble color: https://github.com/daattali/colourpicker
 # 4. ( ) Adjust the size 
 # 5. ( ) Choose from the color template: https://drsimonj.svbtle.com/creating-corporate-colour-palettes-for-ggplot2
 
@@ -36,6 +38,7 @@ subsidy.summary$ID <- tolower(subsidy.summary$State)
 # Shiny R -----------------------------------------------------------------
 
 library(shiny)
+library(colourpicker)
 
 
 ui <- fluidPage(
@@ -50,9 +53,11 @@ ui <- fluidPage(
                   label = "Choose a variable to display",
                   choices = c("Share of foreign investment", 
                               "Share of foreign mega deals"),
-                  selected = "Share of foreign investment")
-      ),
+                  selected = "Share of foreign investment"),
     
+      colourInput("low", "Choose a color for low value","#deebf7"),
+      colourInput("high", "Choose a color for high value", "#08519c")
+    ),
     mainPanel(plotOutput("map"))
   )
   )
@@ -64,11 +69,10 @@ server <- function(input, output) {
     data <- switch(input$var, 
                    "Share of foreign investment" = subsidy.summary$foreign.share, 
                    "Share of foreign mega deals" = subsidy.summary$mega.foreign.share)
-    
     ggplot() + 
-      geom_map(data = states, map = fifty_states, aes(x = long, y = lat, map_id = region),fill = "white", color = "black") +
+      geom_map(data = states, map = fifty_states, aes(x = long, y = lat, map_id = region),fill = "white", color = "grey") +
       geom_map(data = subsidy.summary, map = fifty_states, aes(fill = data, map_id = ID)) +
-      #  scale_fill_continuous(name = var,low='#deebf7', high='#08306b', guide='colorbar') + 
+      scale_fill_gradient(low = input$low, high = input$high) + 
       labs(x=NULL, y=NULL) + 
       coord_map("albers", lat0 = 39, lat1 = 45) + 
       theme(panel.border = element_blank()) + 
